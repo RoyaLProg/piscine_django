18d8416c638e0fa414ba9378b825214ead3c10f8
Camille CAMBIUM <ccambium@f6r6s3.clusters.42paris.fr>
2025-05-13 15:32:07 +0200

    add : django_oob/ex01

---
+++ django0_oob/ex01/intern.py
@@ -0,0 +1,35 @@
+class Intern:
+    def __init__(self, name="My name? I’m nobody, an intern, I have no name."):
+        self.name = name
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Coffee:
+        def __str__(self):
+            return "This is the worst coffee you ever tasted."
+
+    def work(self):
+        raise Exception("I’m just an intern, I can’t do that...")
+
+    def make_coffee(self):
+        return self.Coffee()
+
+
+def main():
+    a = Intern()
+    mark = Intern("Mark")
+
+    print(a)
+    print(mark)
+    myCoffee = mark.make_coffee()
+    print(myCoffee)
+    try:
+        a.work()
+    except Exception as e:
+        print(e)
+
+
+
+if __name__ == "__main__":
+    main()
