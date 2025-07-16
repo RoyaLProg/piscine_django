c8c461bd5c6013749778c8138f0116234cbae19f
Camille CAMBIUM <ccambium@f2r5s12.clusters.42paris.fr>
2025-07-16 14:57:30 +0200

    add : django_lib/ex02

---
+++ django1_lib/ex02/request_wikipedia.py
@@ -0,0 +1,58 @@
+import requests
+import dewiki
+import sys
+
+API_URL = "https://en.wikipedia.org/w/api.php"
+USAGE = f"Usage : {sys.argv[0]} \"query\""
+
+
+def write_to_file(pageTitle: str, content: str):
+    filename = sys.argv[1].lower().replace(" ", "_") + ".wiki"
+    unwikied_content = dewiki.from_string(content)
+    file = open(filename, "w")
+
+    file.write(unwikied_content)
+    file.close()
+
+
+def get_page(pageTitle: str):
+    res = requests.api.get("https://en.wikipedia.org/w/index.php",
+                           [("title", pageTitle), ("action", "raw")])
+
+    if not res.ok:
+        print("page does not exist")
+        return
+
+    content = res.text
+    write_to_file(pageTitle, content)
+
+
+def search(string: str):
+    res = requests.api.get(API_URL, [("action", "query"), ("list", "search"),
+                                     ("srsearch", string), ("format", "json")])
+    if not res.ok:
+        print("request failed")
+        return
+
+    try:
+        res_json = res.json()
+        pageTitle = res_json["query"]["search"][0]["title"]
+        get_page(pageTitle)
+    except ValueError:
+        print("response did not contain valid json")
+        return
+    except IndexError:
+        print("no result found")
+        return
+
+
+def main():
+    if len(sys.argv) != 2:
+        print(USAGE)
+        return
+    query = sys.argv[1]
+    search(query)
+
+
+if __name__ == "__main__":
+    main()
