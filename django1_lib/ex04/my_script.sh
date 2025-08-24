c3e5b89f3c2b45a81273421fac535b8c67644dec
Camille CAMBIUM <royalfr@protonmail.com>
2025-08-24 09:19:50 +0200

    add : django_lib/ex04

---
+++ django1_lib/ex04/my_script.sh
@@ -0,0 +1,5 @@
+if [[ "$SHELL" = "/usr/bin/fish" ]]; then
+	/usr/bin/fish -c "python -m venv django_venv && source $PWD/django_venv/bin/activate.fish && pip install -r requirements.txt && fish"
+else
+	/bin/bash -c "python -m venv django_venv && source $PWD/django_venv/bin/activate && pip install -r requirements.txt && bash"
+fi
